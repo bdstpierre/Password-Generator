5e8bbeeae5ef8c3f90b72069f10d9e4b44f38846
bdstpierre <barry@stpierre.com>
2021-04-12 09:52:28 -0400

    Discovered a bug was using mySet.length instead of allChars[mySet].length

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,11 @@
 // Setup object to hold all characters
 var allChars = {
 
-  specChar: [ " ", "!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-",
-              ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`",
-              "{", "|", "}", "~"],
+  specChar: [ " ", "!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
 
-  specCharCode: [ "\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", 
-                  "\u0027", "\u0028", "\u0029", "\u002A", "\u002B", "\u002C", "\u002D",
-                  "\u002E", "\u002F", "\u003A", "\u003B", "\u003C", "\u003D", "\u003E",
-                  "\u003F", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u005F", 
-                  "\u0060", "\u007B", "\u007C", "\u007D", "\u007E"],
+  lowerChar: [  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
-  lowerChar: [  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-
-  upperChar: [  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
-                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ],
+  upperChar: [  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ],
 
   numChar: [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
 }
@@ -46,10 +36,6 @@ function generatePassword(){
 
   // Determine the password based on the criteria
   myPassword = randomPass(passLength, finalSet);
-
-
-  //test code (replace with actual password when complete)
-  console.log(myPassword);
  
   return myPassword;
 };
@@ -107,10 +93,11 @@ function randomPass(count, chars){
   // for each character in the password, we will randomly select
   // a character set to use and then randomly choose a character
   // from that set
+
   var myPassword = "";
   for (var i = 0; i < count; i++){
     var mySet = chars[Math.floor(Math.random() * chars.length)];
-    var myChar = allChars[mySet][Math.floor(Math.random() * mySet.length)];
+    var myChar = allChars[mySet][Math.floor(Math.random() * allChars[mySet].length)];
 
     myPassword += myChar;
   }
