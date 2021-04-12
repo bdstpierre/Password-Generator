32b4108b4c4180d3df905636a8357a80a040a358
bdstpierre <barry@stpierre.com>
2021-04-11 20:34:28 -0400

    Committing finished code:

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,24 @@
-// Variable
-var specChar = [ " ", "!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-",
-                 ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`",
-                 "{", "|", "}", "~"];
-var specCharCode = [ "\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", 
-                     "\u0027", "\u0028", "\u0029", "\u002A", "\u002B", "\u002C", "\u002D",
-                     "\u002E", "\u002F", "\u003A", "\u003B", "\u003C", "\u003D", "\u003E",
-                     "\u003F", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u005F", 
-                     "\u0060", "\u007B", "\u007C", "\u007D", "\u007E"];
+// Setup object to hold all characters
+var allChars = {
 
-var lowerChar = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-                  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  specChar: [ " ", "!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-",
+              ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`",
+              "{", "|", "}", "~"],
 
-var upperChar = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
-                  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+  specCharCode: [ "\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", 
+                  "\u0027", "\u0028", "\u0029", "\u002A", "\u002B", "\u002C", "\u002D",
+                  "\u002E", "\u002F", "\u003A", "\u003B", "\u003C", "\u003D", "\u003E",
+                  "\u003F", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u005F", 
+                  "\u0060", "\u007B", "\u007C", "\u007D", "\u007E"],
 
-var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+  lowerChar: [  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
+  upperChar: [  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ],
+
+  numChar: [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+}
 
 
 // Assignment Code
@@ -42,13 +45,13 @@ function generatePassword(){
   finalSet = queryCharacters();
 
   // Determine the password based on the criteria
-  password = randomPass(passLength, finalSet);
+  myPassword = randomPass(passLength, finalSet);
 
 
   //test code (replace with actual password when complete)
-  console.log(password);
+  console.log(myPassword);
  
-  return password;
+  return myPassword;
 };
 
 // Query for length of the password
@@ -104,14 +107,14 @@ function randomPass(count, chars){
   // for each character in the password, we will randomly select
   // a character set to use and then randomly choose a character
   // from that set
-  console.log(count, chars);
+  var myPassword = "";
   for (var i = 0; i < count; i++){
-    console.log(i);
-    mySet = chars[Math.floor(Math.random() * chars.length)];
-    console.log(mySet);
+    var mySet = chars[Math.floor(Math.random() * chars.length)];
+    var myChar = allChars[mySet][Math.floor(Math.random() * mySet.length)];
 
+    myPassword += myChar;
   }
 
-  return "string";
+  return myPassword;
 
 }
